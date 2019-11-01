5a06f68424cc128b24142e044d6451d4b2be6e49
Anurag Angara <anurag.angara@gmail.com>
2019-11-01 16:39:17 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EthCrypto from 'eth-crypto'
+
+const Box = require('3box')
 
 function App() {
-  return (
+
+const [entropy, setEntropy] = useState()
+function entropySetter(e) {
+    setEntropy(e)
+}
+// salt to meet min entropy size limits
+const salt = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+async function createBox() {
+    console.log(entropy)
+    let entropyBuffer = Buffer.from(entropy + salt)
+    let account = EthCrypto.createIdentity(entropyBuffer)
+    let ethereum = {
+        sendAsync: function (data, callback) {
+            console.log('data', data)
+            console.log('data params', data.params)
+            console.log('data params[0]', data.params[0])
+            let messageHash = EthCrypto.hash.keccak256(data.params[0]);
+            let signature = EthCrypto.sign(account.privateKey, messageHash)
+            callback(null, {result: signature})
+            return signature
+        }
+    }
+    let box = await Box.openBox(account.address, ethereum)
+    console.log(box)
+}
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>Input entropy below</p>
+    <input onChange={(e) => entropySetter(e.target.value)} type="text" />
+    <button onClick={createBox}>
+        Create new keypair
+    </button>
     </div>
-  );
+    );
 }
 
 export default App;
